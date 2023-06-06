8c5261d143f3988ca63d0fe588e2615fb93ed3df
Jonathan Park <phoenix1469@gmail.com>
2023-06-06 13:24:18 -0700

    jonathan practice.js

---
+++ practice.js
@@ -0,0 +1,9 @@
+const parks = [
+  { name: "Biscayne", rating: 4.2 },
+  { name: "Grand Canyon", rating: 5 },
+  { name: "Gateway Arch", rating: 4.5 },
+  { name: "Indiana Dunes", rating: 4.1 },
+];
+
+parks.sort((parkA, parkB) => (parkA.rating > parkB.rating ? 1 : -1));
+console.log(parks);
